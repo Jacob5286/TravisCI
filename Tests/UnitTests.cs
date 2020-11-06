7e39834258173553dc0de80895e452dc2b5932fd
Jacob(Jiakai) <65819828+Jacob5286@users.noreply.github.com>
2020-11-06 16:12:11 -0600

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -11,7 +11,7 @@ namespace TravisCILab
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
-            Assert.AreEqual(12, Program.Add("5", "7"));
+            Assert.AreEqual(11, Program.Add("5", "7"));
         }
 
         [Test]
@@ -34,7 +34,7 @@ namespace TravisCILab
         
         public void Subtract_Valid()
         {
-            Assert.AreEqual(3, Program.Subtract("5", "2"));
+            Assert.AreEqual(1, Program.Subtract("5", "2"));
             Assert.AreEqual(5, Program.Subtract("7", "2"));
             Assert.AreEqual(12, Program.Subtract("19", "7"));
         }
